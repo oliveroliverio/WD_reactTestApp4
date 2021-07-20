e2c23457c4ac09bd5727cc8f97cead0122f425a5
Oliver <oliveroliverio@gmail.com>
2021-07-19 18:27:58 -0700

    Added first component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import ExpenseItem from './components/ExpenseItem';
 import './App.css';
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
   <div>
     <h2>Title</h2>
     <p>Description text</p>
+    <ExpenseItem></ExpenseItem>
   </div>
   )
 }
