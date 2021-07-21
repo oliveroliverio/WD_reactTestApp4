9bb66a828227b85d1b4c29fee59694f183171479
Oliver <oliveroliverio@gmail.com>
2021-07-20 18:58:29 -0700

    moving variables out of component and into main App component

--- src/App.js
+++ src/App.js
@@ -3,9 +3,15 @@ import ExpenseItem from './components/ExpenseItem';
 import './App.css';
 
 function App() {
+  const expenses = [
+    {title: 'Spotify', amount: 9.99, date: new Date(1985, 11, 15)},
+    {title: 'Spotify', amount: 9.99, date: new Date(1985, 11, 15)},
+    {title: 'Spotify', amount: 9.99, date: new Date(1985, 11, 15)},
+    {title: 'Spotify', amount: 9.99, date: new Date(1985, 11, 15)},
+  ];
   return (
   <div>
-    <h2>Title</h2>
+    <h2>Expenses</h2>
     <p>Description text</p>
     <ExpenseItem></ExpenseItem>
     <ExpenseItem></ExpenseItem>
