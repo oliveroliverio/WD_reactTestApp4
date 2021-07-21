1d11dfc7027c0c9f5c8278dad85de8e8f9bb52ce
Oliver <oliveroliverio@gmail.com>
2021-07-20 18:59:21 -0700

    using actual expenses

--- src/App.js
+++ src/App.js
@@ -4,11 +4,27 @@ import './App.css';
 
 function App() {
   const expenses = [
-    {title: 'Spotify', amount: 9.99, date: new Date(1985, 11, 15)},
-    {title: 'Spotify', amount: 9.99, date: new Date(1985, 11, 15)},
-    {title: 'Spotify', amount: 9.99, date: new Date(1985, 11, 15)},
-    {title: 'Spotify', amount: 9.99, date: new Date(1985, 11, 15)},
+    {
+      id: 'e1',
+      title: 'Spotify',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
   ];
+   
   return (
   <div>
     <h2>Expenses</h2>
