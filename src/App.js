5435674c54c61236a0fcf17509f11ae30d63b74a
Oliver <oliveroliverio@gmail.com>
2021-07-22 09:58:21 -0700

    found the problem, expenseItem component calling in app.js, title, amount, date needed to be within <ExpenseItem ... >

--- src/App.js
+++ src/App.js
@@ -8,24 +8,24 @@ function App() {
       id: 'e1',
       title: 'Spotify',
       amount: 94.12,
-      date:  Date(1998, 3, 15)
+      date: new Date(1998, 3, 15),
     },
     { id: 'e2', 
     title: 'New TV', 
     amount: 799.49, 
-    date:  Date(2013, 3, 15)
+    date: new Date(2013, 3, 15),
     },
     {
       id: 'e3',
       title: 'Car Insurance',
       amount: 294.67,
-      date:  Date(1992, 3, 15)
+      date: new Date(1992, 3, 15),
     },
     {
       id: 'e4',
       title: 'New Desk (Wooden)',
       amount: 450,
-      date:  Date(2021, 3, 15)
+      date: new Date(2021, 3, 15),
     },
   ];
 
@@ -34,27 +34,26 @@ function App() {
   return (
   <div>
     <h2>Expenses</h2>
-    <p>Description text</p>
-    <ExpenseItem>
+    <ExpenseItem
       title={expenses[0].title}
       amount={expenses[0].amount}
       date={expenses[0].date}
-    </ExpenseItem>
-    <ExpenseItem>
+    ></ExpenseItem>
+    <ExpenseItem
       title={expenses[1].title}
       amount={expenses[1].amount}
       date={expenses[1].date}
-    </ExpenseItem>
-    <ExpenseItem>
+    ></ExpenseItem>
+    <ExpenseItem
       title={expenses[2].title}
       amount={expenses[2].amount}
       date={expenses[2].date}
-    </ExpenseItem>
-    <ExpenseItem>
+    ></ExpenseItem>
+    <ExpenseItem
       title={expenses[3].title}
       amount={expenses[3].amount}
       date={expenses[3].date}
-    </ExpenseItem>
+    ></ExpenseItem>
   </div>
   )
 }
