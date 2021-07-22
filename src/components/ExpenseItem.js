9c0f881259296e2797ec6b42972ab52be688c2dd
Oliver <oliveroliverio@gmail.com>
2021-07-21 20:58:26 -0700

    added expenseItem.css

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -5,7 +5,7 @@ function ExpenseItem(props) {
     // const amount = 4.44
     return (
         <div className="expense-item">
-            <div>{props.date.toISOString()}</div>
+            <div>{props.date.toISOString('en-US', {month: 'long'})}</div>
             <div className="expense-item__description">
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
