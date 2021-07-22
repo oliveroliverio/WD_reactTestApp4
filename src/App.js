e5c31e92785fe69cef1e710a2c64adbb256d177e
Oliver <oliveroliverio@gmail.com>
2021-07-21 20:39:27 -0700

    this isn't working for some reason, error: cannot read property 'toISOString' of undefined

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import logo from './logo.svg';
-import ExpenseItem from './components/ExpenseItem';
-import './App.css';
+import ExpenseItem from './components/ExpenseItem'
+import './App.css'
 
 function App() {
   const expenses = [
@@ -8,20 +7,24 @@ function App() {
       id: 'e1',
       title: 'Spotify',
       amount: 94.12,
-      date: new Date(2020, 7, 14),
+      date: new Date(2021, 3, 15)
     },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    { id: 'e2', 
+    title: 'New TV', 
+    amount: 799.49, 
+    date: new Date(2021, 3, 15)
+    },
     {
       id: 'e3',
       title: 'Car Insurance',
       amount: 294.67,
-      date: new Date(2021, 2, 28),
+      date: new Date(2021, 3, 15)
     },
     {
       id: 'e4',
       title: 'New Desk (Wooden)',
       amount: 450,
-      date: new Date(2021, 5, 12),
+      date: new Date(2021, 3, 15)
     },
   ];
    
@@ -29,10 +32,26 @@ function App() {
   <div>
     <h2>Expenses</h2>
     <p>Description text</p>
-    <ExpenseItem></ExpenseItem>
-    <ExpenseItem></ExpenseItem>
-    <ExpenseItem></ExpenseItem>
-    <ExpenseItem></ExpenseItem>
+    <ExpenseItem>
+      title={expenses[0].title}
+      amount={expenses[0].amount}
+      date={expenses[0].date}
+    </ExpenseItem>
+    <ExpenseItem>
+      title={expenses[1].title}
+      amount={expenses[1].amount}
+      date={expenses[1].date}
+    </ExpenseItem>
+    <ExpenseItem>
+      title={expenses[2].title}
+      amount={expenses[2].amount}
+      date={expenses[2].date}
+    </ExpenseItem>
+    <ExpenseItem>
+      title={expenses[3].title}
+      amount={expenses[3].amount}
+      date={expenses[3].date}
+    </ExpenseItem>
   </div>
   )
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,18 @@
-import './ExpenseItem.css';
-function ExpenseItem() {
-    const expenseDate = new Date(2021, 2, 28);
-    const expenseTitle = 'Spotify';
-    const expenseAmount = 9.99;
-
+import './ExpenseItem.css'
+function ExpenseItem(props) {
+    // const expenseDate = new Date(2021, 2, 28)
+    // const expenseTitle = "asdf"
+    // const amount = 4.44
     return (
         <div className="expense-item">
-            <div>{expenseDate.toISOString()}</div>
-            <div className="expense-item__title">
-                <h2>{expenseTitle}</h2>
-                <div className="expense-item__price">${expenseAmount}</div>
+            <div>{props.date.toISOString()}</div>
+            <div className="expense-item__description">
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">${props.amount}</div>
             </div>
         </div>
-    );
+
+    )
 }
 
 export default ExpenseItem
