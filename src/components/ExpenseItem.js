6b30cdddb46977548f7a3566979ec6782a20b9e2
Oliver <oliveroliverio@gmail.com>
2021-07-22 17:30:52 -0700

    added a more readable date

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,15 @@
 import './ExpenseItem.css'
 function ExpenseItem(props) {
-    // const expenseDate = new Date(1998, 3, 2)
-    // const expenseTitle = "asdf"
-    // const amount = 4.44
+    const month = props.date.toLocaleString('en-US', {month: 'long'})
+    const day = props.date.toLocaleString('en-US', { day: '2-digit'})
+    const year = props.date.getFullYear()
     return (
         <div className="expense-item">
-            <div>{props.date.toString()}</div>
+            <div>
+                <div>{month}</div>
+                <div>{year}</div>
+                <div>{day}</div>
+            </div>
             <div className="expense-item__description">
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
