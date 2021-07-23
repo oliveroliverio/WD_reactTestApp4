a649f3c89bfa2a90c70211cfe560743d15d49045
Oliver <oliveroliverio@gmail.com>
2021-07-22 18:40:37 -0700

    Made date look nicer with CSS

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -1,4 +1,5 @@
 
+import './ExpenseDate.css'
 function ExpenseDate(props) {
     const month = props.date.toLocaleString('en-US', {month: 'long'})
     const day = props.date.toLocaleString('en-US', { day: '2-digit'})
@@ -6,9 +7,9 @@ function ExpenseDate(props) {
 
     return (
         <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
+        <div className='expense-date__month'>{month}</div>
+        <div className='expense-date__year'>{year}</div>
+        <div className='expense-date__day'>{day}</div>
     </div>
     )
 }
