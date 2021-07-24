98312d6486f9bf3599d33ee304c1520892b06f21
Oliver <oliveroliverio@gmail.com>
2021-07-24 14:07:51 -0700

    Now title is set to a variable, which can change when button is clicked

--- src/components/Expense/ExpenseItem.js
+++ src/components/Expense/ExpenseItem.js
@@ -2,6 +2,7 @@ import './ExpenseItem.css'
 import Card from '../UI/Card' 
 import ExpenseDate from '../Expense/ExpenseDate'
 function ExpenseItem(props) {
+    let title = props.title
     function clickHandler() {
         console.log("clicked (but using click handler!!!")
     }
@@ -9,7 +10,7 @@ function ExpenseItem(props) {
         <Card className="expense-item">
             <ExpenseDate date={props.date} />
             <div className="expense-item__description">
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
             </div>
             <button onClick={clickHandler}>Change Title</button>
