4abea210d4379bf69b7e4e6eee23c23b8f4527f9
Oliver <oliveroliverio@gmail.com>
2021-07-24 14:01:57 -0700

    Added a button that displays 'clicked' on console log when clicked, for testing

--- src/components/Expense/ExpenseItem.js
+++ src/components/Expense/ExpenseItem.js
@@ -9,6 +9,7 @@ function ExpenseItem(props) {
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
             </div>
+            <button onClick={() => {console.log('clicked!!!')}}>Change Title</button>
         </Card>
 
     )
