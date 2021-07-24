0f32cb3467231c3489021490bf33f49c951d576a
Oliver <oliveroliverio@gmail.com>
2021-07-24 14:05:45 -0700

    created a event handler for clicked instead of putting it directly into JSX code

--- src/components/Expense/ExpenseItem.js
+++ src/components/Expense/ExpenseItem.js
@@ -2,6 +2,9 @@ import './ExpenseItem.css'
 import Card from '../UI/Card' 
 import ExpenseDate from '../Expense/ExpenseDate'
 function ExpenseItem(props) {
+    function clickHandler() {
+        console.log("clicked (but using click handler!!!")
+    }
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date} />
@@ -9,7 +12,7 @@ function ExpenseItem(props) {
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
             </div>
-            <button onClick={() => {console.log('clicked!!!')}}>Change Title</button>
+            <button onClick={clickHandler}>Change Title</button>
         </Card>
 
     )
